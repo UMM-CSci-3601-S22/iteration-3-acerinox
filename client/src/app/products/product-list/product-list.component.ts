02e1ba34692eae035cea4cdebca4e199bf061137
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-03-31 16:42:54 -0500

    Start refactor product-list.compenent.ts
    Aim is to loop over product categoryNameMap
    instead of hard coded values
    
    Co-Authored-By: Daniel Albright <albri118@morris.umn.edu>

--- client/src/app/products/product-list/product-list.component.ts
+++ client/src/app/products/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, TemplateRef, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -5,8 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Product, ProductCategory } from '../product';
 import { ProductService } from '../product.service';
 import { Subscription } from 'rxjs';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-product-list-component',
@@ -20,11 +20,11 @@ export class ProductListComponent implements OnInit, OnDestroy {
   @ViewChild('dialogRef')
   dialogRef!: TemplateRef<any>;
 
-  public serverFilteredProducts: Product[];
-  public filteredProducts: Product[];
+  public serverFilteredProducts: Product[] = [];
+  public filteredProducts: Product[] = [];
 
   // Unfiltered product list
-  public allProducts: Product[];
+  public allProducts: Product[] = [];
 
   public name: string;
   public productBrand: string;
@@ -37,66 +37,30 @@ export class ProductListComponent implements OnInit, OnDestroy {
   // Boolean for if there are active filters
   public activeFilters: boolean;
 
-  // Category collections for use in displaying product categories
-  public bakeryProducts: Product[];
-  public produceProducts: Product[];
-  public meatProducts: Product[];
-  public dairyProducts: Product[];
-  public frozenProducts: Product[];
-  public cannedProducts: Product[];
-  public drinkProducts: Product[];
-  public generalProducts: Product[];
-  public seasonalProducts: Product[];
-  public miscellaneousProducts: Product[];
+  // A list of the categories to be displayed, requested by the customer
+  public categories: ProductCategory[] = [
+    'bakery',
+    'produce',
+    'meat',
+    'dairy',
+    'frozen foods',
+    'canned goods',
+    'drinks',
+    'general grocery',
+    'seasonal',
+  ];
+
+  // Stores the products sorted by their category
+  public categoryNameMap = new Map<ProductCategory, Product[]>();
 
   // temp variables to use for deletion
   public tempId: string;
   public tempName: string;
   public tempDialog: any;
   public tempDeleted: Product;
+  asyncResult: any;
   constructor(private productService: ProductService, private snackBar: MatSnackBar, public dialog: MatDialog) { }
 
-  openDeleteDialog(pname: string, id: string) {
-    this.tempId = id;
-    this.tempName = pname;
-    this.tempDialog = this.dialog.open(this.dialogRef, { data: {name: this.tempName, _id: this.tempId} }, );
-    this.tempDialog.afterClosed().subscribe((res) => {
-
-      // Data back from dialog
-      console.log({ res });
-    });
-  }
-
-  getUnfilteredProducts(): void {
-    this.unsubUnfiltered();
-    this.getUnfilteredProductsSub = this.productService.getProducts().subscribe(returnedProducts => {
-      this.allProducts = returnedProducts;
-      this.makeCategoryLists();
-    });
-  }
-
-  public makeCategoryLists(): void {
-    this.bakeryProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'bakery'});
-    this.produceProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'produce'});
-    this.meatProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'meat'});
-    this.dairyProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'dairy'});
-    this.frozenProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'frozen foods'});
-    this.cannedProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'canned goods'});
-    this.drinkProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'drinks'});
-    this.generalProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'general grocery'});
-    this.seasonalProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'seasonal'});
-    this.miscellaneousProducts = this.productService.filterProducts(
-      this.allProducts, { category: 'miscellaneous'});
-  }
   getProductsFromServer(): void {
     this.unsub();
     this.getProductsSub = this.productService.getProducts({
@@ -116,23 +80,52 @@ export class ProductListComponent implements OnInit, OnDestroy {
     }
   }
 
-  public updateFilter(): void {
-    this.filteredProducts = this.productService.filterProducts(
-      this.serverFilteredProducts, { product_name: this.name, brand: this.productBrand , limit: this.productLimit });
-      if (this.name || this.productBrand || this.productCategory || this.productStore) {
-        this.activeFilters = true;
-      }
-      else {
-        this.activeFilters = false;
-      }
+  getUnfilteredProducts(): void {
+    this.getUnfilteredProductsSub = this.productService.getProducts().subscribe(returnedProducts => {
+      this.allProducts = returnedProducts;
+    });
+    console.log(this.allProducts);
+  }
 
+  // Sorts products based on their category
+  initializeCategoryMap() {
+    for (let givenCategory of this.categories) {
+      this.categoryNameMap.set(givenCategory,
+        this.productService.filterProducts(this.serverFilteredProducts, { category: givenCategory }));
+
+    }
+    console.log(this.categoryNameMap);
+  }
+
+  openDeleteDialog(pname: string, id: string) {
+    this.tempId = id;
+    this.tempName = pname;
+    this.tempDialog = this.dialog.open(this.dialogRef, { data: { name: this.tempName, _id: this.tempId } },);
+    this.tempDialog.afterClosed().subscribe((res) => {
+
+      // Data back from dialog
+      console.log({ res });
+    });
   }
 
 
 
+  public updateFilter(): void {
+    this.filteredProducts = this.productService.filterProducts(
+      this.serverFilteredProducts, { product_name: this.name, brand: this.productBrand, limit: this.productLimit });
+    if (this.name || this.productBrand || this.productCategory || this.productStore) {
+      this.activeFilters = true;
+    }
+    else {
+      this.activeFilters = false;
+    }
+
+  }
+
   ngOnInit(): void {
     this.getProductsFromServer();
     this.getUnfilteredProducts();
+    this.initializeCategoryMap();
   }
 
   ngOnDestroy(): void {
@@ -145,30 +138,14 @@ export class ProductListComponent implements OnInit, OnDestroy {
     }
   }
 
-  unsubUnfiltered(): void {
-    if (this.getUnfilteredProductsSub) {
-      this.getUnfilteredProductsSub.unsubscribe();
-    }
-  }
-
+  // Removes the product and updates the categoryNameMap to reflect the deletion
   removeProduct(id: string): Product {
     this.productService.deleteProduct(id).subscribe(
       prod => {
         this.allProducts = this.allProducts.filter(product => product._id !== id);
-        this.filteredProducts = this.filteredProducts.filter(product => product._id !== id);
-        this.serverFilteredProducts = this.filteredProducts.filter(product => product._id !== id);
-        this.bakeryProducts = this.bakeryProducts.filter(product => product._id !== id);
-        this.produceProducts = this.produceProducts.filter(product => product._id !== id);
-        this.meatProducts = this.meatProducts.filter(product => product._id !== id);
-        this.dairyProducts = this.dairyProducts.filter(product => product._id !== id);
-        this.frozenProducts = this.frozenProducts.filter(product => product._id !== id);
-        this.cannedProducts = this.cannedProducts.filter(product => product._id !== id);
-        this.drinkProducts = this.drinkProducts.filter(product => product._id !== id);
-        this.generalProducts = this.generalProducts.filter(product => product._id !== id);
-        this.seasonalProducts = this.seasonalProducts.filter(product => product._id !== id);
-        this.miscellaneousProducts = this.miscellaneousProducts.filter(product => product._id !== id);
         this.tempDeleted = prod;
-     }
+        this.initializeCategoryMap();
+      }
     );
     this.tempDialog.close();
     this.snackBar.open('Product deleted', 'OK', {
