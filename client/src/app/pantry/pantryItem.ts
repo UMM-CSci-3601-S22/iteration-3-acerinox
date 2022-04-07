51504ed2de48659cfd26f23aa252f0aed3a0944e
Daniel Albright <albri118@morris.umn.edu>
2022-04-07 00:20:29 -0500

    Add ComboItem interface
    Using this as a combination of keyvalues we want
    to use for displaying infomation in the pantry
    
    Co-Authored-By: Dongting Cai <cai00151@morris.umn.edu>

--- client/src/app/pantry/pantryItem.ts
+++ client/src/app/pantry/pantryItem.ts
@@ -1,3 +1,5 @@
+import { ProductCategory } from '../products/product';
+
 export interface PantryItem {
   _id: string;
   product: string;
@@ -5,4 +7,21 @@ export interface PantryItem {
   purchase_date: string;
   notes?: string;
 
+}
+
+export interface ComboItem {
+  _id: string; // PantryItem
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  purchase_date: string; // PantryItem
+  productName: string; // Product
+  description?: string; // Product
+  brand: string; // Product
+  category: ProductCategory; // Product
+  store: string; // Product
+  location?: string; // Product
+  notes?: string; // PantryItem
+  tags?: string[]; // Product
+  lifespan?: number; // Product
+  threshold?: number; // Product
+  image?: string; // Product
 }
