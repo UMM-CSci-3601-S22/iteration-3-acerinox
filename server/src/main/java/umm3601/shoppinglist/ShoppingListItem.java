9b46549a5c96fdef2a1df0a81f3d7389bbce8383
Yubo <mao00071@morris.umn.edu>
2022-04-14 16:30:43 -0500

    Add shoppingListItem interface
    Define the fields for the shoppingListItem.
    Co-authored-by: jpwalbran <walbr042@morris.umn.edu>

---
+++ server/src/main/java/umm3601/shoppinglist/ShoppingListItem.java
@@ -0,0 +1,15 @@
+package umm3601.shoppinglist;
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({ "VisibilityModifier", "MemberName" })
+public class ShoppingListItem {
+  @ObjectId
+  @Id
+  // By default Java field names shouldn't start with underscores.
+  // Here, though, we *have* to use the name `_id` to match the
+  // name of the field as used by MongoDB.
+  public String _id;
+  public String product;
+  public int count;
+}
