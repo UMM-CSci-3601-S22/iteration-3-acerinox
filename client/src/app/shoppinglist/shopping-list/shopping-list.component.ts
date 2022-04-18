ed0c5e727451e547e9f64199d58cef064841c6bd
Daniel Albright <albri118@morris.umn.edu>
2022-04-18 11:54:55 -0500

    Start shoppinglist component logic
    Grabbing the ShoppingListDisplayItems
    for displaying on the html template later

--- client/src/app/shoppinglist/shopping-list/shopping-list.component.ts
+++ client/src/app/shoppinglist/shopping-list/shopping-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppinglistDisplayItem } from '../shoppinglistDisplayItem';
+import { ShoppinglistService } from '../shoppinglist.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  public shoppingList: ShoppinglistDisplayItem[];
+  getShoppinglistSub: Subscription;
 
-  constructor() { }
+  constructor(private shoppinglistService: ShoppinglistService) {}
+
+  public getShoppinglistFromServer(): void {
+    this.unsub();
+    this.getShoppinglistSub = this.shoppinglistService.getShoppinglist()
+      .subscribe(returnedShoppinglist => {
+        this.shoppingList = returnedShoppinglist;
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  unsub(): void {
+    if (this.getShoppinglistSub) {
+      this.getShoppinglistSub.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {
   }
