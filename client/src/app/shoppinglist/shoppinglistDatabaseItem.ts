05a273afc11e3925052a0c369a3d00bda66a42f5
Daniel Albright <albri118@morris.umn.edu>
2022-04-18 11:52:23 -0500

    Separate ShoppinglistItem into display and db
    DisplayItem will be what is shown on the shoppinglist, come form GET
    
    DatabaseItem is what POST and REMOVE will use (add and delete)

---
+++ client/src/app/shoppinglist/shoppinglistDatabaseItem.ts
@@ -0,0 +1,5 @@
+export interface ShoppinglistDatabaseItem {
+  _id: string;
+  product: string;
+  count: number;
+}
--- client/src/app/shoppinglist/shoppinglistDisplayItem.ts
+++ client/src/app/shoppinglist/shoppinglistDisplayItem.ts
@@ -1,7 +1,8 @@
-export interface ShoppinglistItem {
+export interface ShoppinglistDisplayItem {
   _id: string;
   product: string;
   name: string;
   store: string;
+  location: string;
   count: number;
 }
