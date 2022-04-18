9c45b105e546d192909d7edd2faff9c6f8101b34
Yubo <mao00071@morris.umn.edu>
2022-04-18 14:21:49 -0500

    fix one merge conflict

--- server/src/test/java/umm3601/shoppinglist/ShoppingListControllerSpec.java
+++ server/src/test/java/umm3601/shoppinglist/ShoppingListControllerSpec.java
@@ -162,8 +162,6 @@ public class ShoppingListControllerSpec {
             .append("threshold", 40));
     productsDocuments.insertMany(testProducts);
 
-
-
     // Add test list for shopping list items to database
     List<Document> testShoppingListItems = new ArrayList<>();
     appleEntryId = new ObjectId();
@@ -189,7 +187,9 @@ public class ShoppingListControllerSpec {
             .append("product", bananaProductId)
             .append("count", 20)
     );
+
     shoppingListDocuments.insertMany(testShoppingListItems);
+  }
 
     shoppingListController = new ShoppingListController(db);
 }
