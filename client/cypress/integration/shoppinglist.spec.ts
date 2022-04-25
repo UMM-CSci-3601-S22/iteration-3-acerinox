3380d1ec2b683d671d7edf99ece73ee19c7f3cd7
Daniel Albright <albri118@morris.umn.edu>
2022-04-25 02:04:07 -0500

    Start cypress tests for shoppinglist

--- client/cypress/integration/shoppinglist.spec.ts
+++ client/cypress/integration/shoppinglist.spec.ts
@@ -6,16 +6,46 @@ describe('Shoppinglist', () => {
   beforeEach(() => {
     page.navigateTo();
   });
+
+  it('Should have the correct url', () => {
+    page.getUrl().should('contain.text', './shoppinglist');
+  });
+
+  it('Should have the correct title', () => {
+    page.getShoppinglistTitle().should('have.text', ' My Shopping List ');
+  });
+
+  it ('Should have a print button on the print view', () => {
+    page.clickPrintViewToggle();
+    page.getPrintButton();
+  });
 });
 
 describe('Interactive Shoppinglist', () => {
   beforeEach(() => {
     page.navigateTo();
   });
+
+  it('Should have the correct store tabs', () => {
+    page.getUrl();
+  });
+
+  it('Should have the correct products', () => {
+    page.getUrl();
+  });
 });
 
 describe('Print Shoppinglist', () => {
   beforeEach(() => {
     page.navigateTo();
+    page.clickPrintViewToggle();
+  });
+
+  it('Should have the correct store lists and products', () => {
+    page.getUrl();
+  });
+
+  it('Should click the print button', () => {
+    page.getUrl();
   });
 });
--- client/cypress/support/shoppinglist.po.ts
+++ client/cypress/support/shoppinglist.po.ts
@@ -12,4 +12,24 @@ export class ShoppinglistPage {
   getShoppinglistTitle() {
     return cy.get('.shoppinglist-title');
   }
+
+  getInteractiveShoppingList() {
+    cy.get('shopping-list-interactive');
+  }
+
+  clickInteractiveViewToggle() {
+    cy.get('#interactive-view-button').click();
+  }
+
+  clickPrintViewToggle() {
+    cy.get('#print-view-button').click();
+  }
+
+  getPrintShoppingList() {
+    cy.get('.shopping-list-print');
+  }
+
+  getPrintButton() {
+    cy.get('.print-button');
+  }
 }
