6fa4194b68c8b4ff454bff3bc8c7d1fcdb75dd9b
Daniel Albright <albri118@morris.umn.edu>
2022-04-28 11:58:26 -0500

    Generate new component for adding to shoppinglist

---
+++ client/src/app/products/product-list/add-product-to-shoppinglist/add-product-to-shoppinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-to-shoppinglist',
+  templateUrl: './add-product-to-shoppinglist.component.html',
+  styleUrls: ['./add-product-to-shoppinglist.component.scss']
+})
+export class AddProductToShoppinglistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
