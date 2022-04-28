81513d2d7a1d55960fa2e52b02cf8c78c7f48802
Daniel Albright <albri118@morris.umn.edu>
2022-04-28 12:24:53 -0500

    connection to add endpoint and ui button
    still need karma tests on the addShoppinglistItem method in the
    shoppinglist service, as well as cypress tests for the button/dialog.

--- client/src/app/products/product-list/add-product-to-shoppinglist/add-product-to-shoppinglist.component.ts
+++ client/src/app/products/product-list/add-product-to-shoppinglist/add-product-to-shoppinglist.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../../product';
+import { ShoppinglistDatabaseItem } from 'src/app/shoppinglist/shoppinglistDatabaseItem';
+import { MAT_DIALOG_DATA, MatDialogRef, } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-product-to-shoppinglist',
   templateUrl: './add-product-to-shoppinglist.component.html',
   styleUrls: ['./add-product-to-shoppinglist.component.scss']
 })
+
 export class AddProductToShoppinglistComponent implements OnInit {
 
-  constructor() { }
+  addToShoppinglistForm: FormGroup;
+  newShoppinglistItem: ShoppinglistDatabaseItem;
 
-  ngOnInit(): void {
+
+  addPantryValidationMessages = {
+    count: [
+      { type: 'required', message: 'Item count is required'},
+      { type: 'min', message: 'Item count must be at least 1' },
+      { type: 'max', message: 'Item count must be less than 1000000' },
+      { type: 'pattern', message: 'Item count must be a whole number' }
+    ]
+  };
+
+
+  constructor(private fb: FormBuilder,
+    public dialogRef: MatDialogRef<AddProductToShoppinglistComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Product) {
   }
 
+  createForms() {
+    this.addToShoppinglistForm = this.fb.group({
+      product: this.data._id,
+
+      count: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.min(1),
+        Validators.max(1000000),
+        Validators.pattern('^[0-9]+$')
+      ])),
+    });
+  }
+
+  ngOnInit(): void {
+    this.createForms();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  submitForm() {
+    return this.addToShoppinglistForm.value;
+  }
 }
--- client/src/app/shoppinglist/shoppinglist.service.ts
+++ client/src/app/shoppinglist/shoppinglist.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { ShoppinglistDatabaseItem } from './shoppinglistDatabaseItem';
 import { ShoppinglistStoreGroup } from './shoppinglistStoreGroup';
 
 @Injectable()
@@ -15,5 +16,10 @@ export class ShoppinglistService {
     return this.httpClient.get<ShoppinglistStoreGroup[]>(this.shoppinglistUrl, {
       params: httpParams,
     });
+  }
+
+  addShoppinglistItem(newShoppinglistItem: ShoppinglistDatabaseItem): Observable<string> {
+    // Send post request to add a new item to the shopping list with data from the request body.
+    return this.httpClient.post<{ id: string }>(this.shoppinglistUrl, newShoppinglistItem).pipe(map(res => res.id));
   }
 }
