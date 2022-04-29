b785935c6f184b8f7c993968174e2e57b533f1d0
Daniel Albright <albri118@morris.umn.edu>
2022-04-29 13:56:50 -0500

    Have the shoppinglist query use/return mongo ids
    Before when just using generic document type, mongoids were not returning
    the ObjectID, but just a timestamp and date.
    
    By defining types that use @ObjectID and @ID this will return the
    wanted hextring value for the query.

--- server/src/main/java/umm3601/shoppinglist/ShoppingListController.java
+++ server/src/main/java/umm3601/shoppinglist/ShoppingListController.java
@@ -23,47 +23,46 @@ import umm3601.product.Product;
 
 public class ShoppingListController {
 
-    private final JacksonMongoCollection<ShoppingListItem> shoppingListCollection;
-    private final JacksonMongoCollection<Product> productCollection;
+  private final JacksonMongoCollection<ShoppingListItem> shoppingListCollection;
+  private final JacksonMongoCollection<Product> productCollection;
 
-    public ShoppingListController(MongoDatabase database) {
-        shoppingListCollection = JacksonMongoCollection.builder().build(
-                database,
-                "shoppingList",
-                ShoppingListItem.class,
-                UuidRepresentation.STANDARD);
-        productCollection = JacksonMongoCollection.builder().build(
-                database,
-                "products",
-                Product.class,
-                UuidRepresentation.STANDARD);
-    }
+  public ShoppingListController(MongoDatabase database) {
+    shoppingListCollection = JacksonMongoCollection.builder().build(
+        database,
+        "shoppingList",
+        ShoppingListItem.class,
+        UuidRepresentation.STANDARD);
+    productCollection = JacksonMongoCollection.builder().build(
+        database,
+        "products",
+        Product.class,
+        UuidRepresentation.STANDARD);
+  }
 
-    public void getAllShoppingListItems(Context ctx) {
-        ArrayList<Document> returnedShoppingListItems = shoppingListCollection
-                .aggregate(
-                        Arrays.asList(
-                                Aggregates.lookup("products", "product", "_id", "productData"),
-                                Aggregates.unwind("$productData"),
-                                Aggregates.group("$productData.store", Accumulators.addToSet("products",
-                                new Document("productName", "$productData.productName")
-                                .append("location", "$productData.location")
-                                .append("count", "$count"))),
-                                Aggregates.project(Projections.fields(
-                                    Projections.computed("store", "$_id"),
-                                    Projections.include("products"),
-                                    Projections.excludeId()
-                                )),
-                                Aggregates.sort(ascending("store"))
-                        ),
-                        Document.class)
-                        .into(new ArrayList<>());
+  public void getAllShoppingListItems(Context ctx) {
+    ArrayList<ShoppingListStoreGroup> returnedShoppingListItems = shoppingListCollection
+        .aggregate(
+            Arrays.asList(
+                Aggregates.lookup("products", "product", "_id", "productData"),
+                Aggregates.unwind("$productData"),
+                Aggregates.group("$productData.store", Accumulators.addToSet("products",
+                    new Document("_id", "$productData._id")
+                        .append("productName", "$productData.productName")
+                        .append("brand", "$productData.brand")
+                        .append("location", "$productData.location")
+                        .append("count", "$count"))),
+                Aggregates.project(Projections.fields(
+                    Projections.computed("store", "$_id"),
+                    Projections.include("products"),
+                    Projections.excludeId())),
+                Aggregates.sort(ascending("store"))),
+            ShoppingListStoreGroup.class)
+        .into(new ArrayList<>());
 
+    ctx.json(returnedShoppingListItems);
+  }
 
-        ctx.json(returnedShoppingListItems);
-    }
-
-    /**
+  /**
    * Checks if the given entry exists with a given id. if no such entry exists
    * returns false. Returns true for one or more entry with a matching
    * id.
@@ -71,47 +70,47 @@ public class ShoppingListController {
    * @param id
    * @return boolean - true if one or more functions with matching names exit.
    */
-    private boolean productExists(String id) {
-      Product product;
+  private boolean productExists(String id) {
+    Product product;
 
-      try {
-        product = productCollection.find(eq("_id", new ObjectId(id))).first();
-      } catch (IllegalArgumentException e) {
-        return false;
-      }
-      if (product == null) {
-        return false;
-      }
-      return true;
+    try {
+      product = productCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      return false;
     }
-
-    public void resetShoppingList(Context ctx) {
-      //placeholder method for the shoppinglist generation
-      ctx.json(null);
+    if (product == null) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-    * Validate then add a received shoppinglist item to the shoppinglist collection
-    *
-    * @param ctx a Javalin HTTP context
-    */
-    @SuppressWarnings({ "MagicNumber" })
-    public void addNewShoppingListItem(Context ctx) {
+  public void resetShoppingList(Context ctx) {
+    // placeholder method for the shoppinglist generation
+    ctx.json(null);
+  }
 
-      ShoppingListItem newShoppingListItem = ctx.bodyValidator(ShoppingListItem.class)
-          .check(item -> productExists(item.product), "error: product does not exist")
-          .check(item -> ObjectId.isValid(item.product), "The product id is not a legal Mongo Object ID.")
-          .check(item -> item.count >= 1,
-              "Shopping list item count cannot be 0")
-          .get();
+  /**
+   * Validate then add a received shoppinglist item to the shoppinglist collection
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  @SuppressWarnings({ "MagicNumber" })
+  public void addNewShoppingListItem(Context ctx) {
 
-      shoppingListCollection.insertOne(newShoppingListItem);
+    ShoppingListItem newShoppingListItem = ctx.bodyValidator(ShoppingListItem.class)
+        .check(item -> productExists(item.product), "error: product does not exist")
+        .check(item -> ObjectId.isValid(item.product), "The product id is not a legal Mongo Object ID.")
+        .check(item -> item.count >= 1,
+            "Shopping list item count cannot be 0")
+        .get();
 
-      // 201 is the HTTP code for when we successfully
-      // create a new resource (a pantry item in this case).
-      // See, e.g., https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      // for a description of the various response codes.
-      ctx.status(HttpCode.CREATED);
-      ctx.json(Map.of("id", newShoppingListItem._id));
-    }
+    shoppingListCollection.insertOne(newShoppingListItem);
+
+    // 201 is the HTTP code for when we successfully
+    // create a new resource (a pantry item in this case).
+    // See, e.g., https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    // for a description of the various response codes.
+    ctx.status(HttpCode.CREATED);
+    ctx.json(Map.of("id", newShoppingListItem._id));
+  }
 }
--- server/src/main/java/umm3601/shoppinglist/ShoppingListStoreGroup.java
+++ server/src/main/java/umm3601/shoppinglist/ShoppingListStoreGroup.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 @SuppressWarnings({ "VisibilityModifier", "MemberName" })
 public class ShoppingListStoreGroup {
-  @ObjectId
-  @Id
+  //@ObjectId
+  //@Id
   // By default Java field names shouldn't start with underscores.
   // Here, though, we *have* to use the name `_id` to match the
   // name of the field as used by MongoDB.
