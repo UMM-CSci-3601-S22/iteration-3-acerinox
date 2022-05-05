f1d7fcef8853142b5d51d62fefdf78989ade2251
Daniel Albright <albri118@morris.umn.edu>
2022-05-04 21:07:39 -0500

    add shoppinglist delete dialog content

--- client/src/app/shoppinglist/shoppinglist-dialog-delete/shoppinglist-dialog-delete.component.ts
+++ client/src/app/shoppinglist/shoppinglist-dialog-delete/shoppinglist-dialog-delete.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ShoppinglistDisplayItem } from '../shoppinglistDisplayItem';
 
 @Component({
   selector: 'app-shoppinglist-dialog-delete',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppinglistDialogDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ShoppinglistDialogDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ShoppinglistDisplayItem) { }
 
   ngOnInit(): void {
   }
