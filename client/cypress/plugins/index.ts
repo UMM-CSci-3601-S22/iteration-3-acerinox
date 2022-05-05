c89c70716e0c7b83a88d7304e4d781521beda67e
Nic McPhee <mcphee@morris.umn.edu>
2022-05-05 18:29:07 -0500

    Add Cypress/E2E test coverage
    This follows [this blog post](https://lukas-klement.medium.com/implementing-code-coverage-with-angular-and-cypress-6ed08ed7e617) and adds code coverage via the Cypress tests.
    
    Something about it isn't quite right, because a bunch of tests that used to pass now fail, and that may be part of why the coverage ain't great.

--- client/cypress/plugins/index.ts
+++ client/cypress/plugins/index.ts
@@ -1,3 +1,4 @@
+import * as registerCodeCoverageTasks from '@cypress/code-coverage/task';
 import {seedAll} from '@floogulinc/cypress-mongo-seeder';
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -25,6 +26,7 @@ const pluginConfig: Cypress.PluginConfig = (on, config) => {
     'seed:database': (drop = true) => seedAll(mongoUri, dbSeedDir, drop),
   });
 
+  return registerCodeCoverageTasks(on, config);
 };
 
 export default pluginConfig;
